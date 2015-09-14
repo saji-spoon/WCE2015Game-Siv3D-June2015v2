e68e8df54a9c22ead4e6fd31008ad05cc3df13c5
saji-spoon <tkgkmny0707@hotmail.co.jp>
2015-09-14 22:40:02 +0900

    Config.hpp

---
+++ WCE2015Game-Siv3D(June2015v2)/Config.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+namespace shimi
+{
+
+struct ConfigParam
+{
+	const static double POP_DISTANCE_IN;
+	const static double POP_DISTANCE_OUT;
+	const static double DEPOP_DISTANCE;	
+	const static int DEPOP_COUNT;
+};
+
+}
